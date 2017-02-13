3e5b1ffff202f8c59e3035bf80876d5590250c2b
Romain Canon <romain.hydrocanon@gmail.com>
2017-02-13 13:55:02 +0100

    [TASK] Change TYPO3 version requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
 
     'constraints' => [
         'depends'   => [
-            'typo3' => '6.2.0-7.99.99',
+            'typo3' => '6.2.0-8.5.99',
             'php'   => '5.5.0-7.99.99'
         ],
         'conflicts' => [],
