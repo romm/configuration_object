f4d59fd335943d16ef78095aceb45464db4e6de5
Romain Canon <romain.hydrocanon@gmail.com>
2017-02-21 22:07:18 +0100

    [FEATURE] Introduce `ClassExtendsValidator`

---
+++ Classes/Validation/Validator/ClassExtendsValidator.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * 2017 Romain CANON <romain.hydrocanon@gmail.com>
+ *
+ * This file is part of the TYPO3 Configuration Object project.
+ * It is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License, either
+ * version 3 of the License, or any later version.
+ *
+ * For the full copyright and license information, see:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Romm\ConfigurationObject\Validation\Validator;
+
+use Romm\ConfigurationObject\Core\Core;
+use TYPO3\CMS\Extbase\Validation\Validator\AbstractValidator;
+
+class ClassExtendsValidator extends AbstractValidator
+{
+
+    /**
+     * @var array
+     */
+    protected $supportedOptions = [
+        'class' => [
+            '',
+            'Name of the class that the original class should extend.',
+            'string',
+            true
+        ]
+    ];
+
+    /**
+     * Checks if the value is an existing class.
+     *
+     * @param mixed $value The value that should be validated.
+     */
+    public function isValid($value)
+    {
+        if (false === Core::get()->classExists($value)) {
+            $errorMessage = $this->translateErrorMessage(
+                'validator.class_exists.not_valid',
+                'configuration_object',
+                [$value]
+            );
+
+            $this->addError($errorMessage, 1487711134);
+        } elseif (false === in_array($this->options['class'], class_parents($value))) {
+            $errorMessage = $this->translateErrorMessage(
+                'validator.class_extends_class.not_valid',
+                'configuration_object',
+                [$value, $this->options['class']]
+            );
+
+            $this->addError($errorMessage, 1487711239);
+        }
+    }
+}
