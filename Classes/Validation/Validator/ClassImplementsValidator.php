9b2208c2529d64271b7a339169dc7440128d81fe
Romain Canon <romain.hydrocanon@gmail.com>
2017-02-21 22:04:32 +0100

    [FEATURE] Introduce `ClassImplementsValidator`

---
+++ Classes/Validation/Validator/ClassImplementsValidator.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * 2017 Romain CANON <romain.hydrocanon@gmail.com>
+ *
+ * This file is part of the TYPO3 Configuration Object project.
+ * It is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License, either
+ * version 3 of the License, or any later version.
+ *
+ * For the full copyright and license information, see:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Romm\ConfigurationObject\Validation\Validator;
+
+use Romm\ConfigurationObject\Core\Core;
+use TYPO3\CMS\Extbase\Validation\Validator\AbstractValidator;
+
+class ClassImplementsValidator extends AbstractValidator
+{
+
+    /**
+     * @var array
+     */
+    protected $supportedOptions = [
+        'interface' => [
+            '',
+            'Name of the interface that the class name should implement.',
+            'string',
+            true
+        ]
+    ];
+
+    /**
+     * Checks if the value is an existing class.
+     *
+     * @param mixed $value The value that should be validated.
+     */
+    public function isValid($value)
+    {
+        if (false === Core::get()->classExists($value)) {
+            $errorMessage = $this->translateErrorMessage(
+                'validator.class_exists.not_valid',
+                'configuration_object',
+                [$value]
+            );
+
+            $this->addError($errorMessage, 1487710245);
+        } elseif (false === in_array($this->options['interface'], class_implements($value))) {
+            $errorMessage = $this->translateErrorMessage(
+                'validator.class_implements_interface.not_valid',
+                'configuration_object',
+                [$value, $this->options['interface']]
+            );
+
+            $this->addError($errorMessage, 1487710370);
+        }
+    }
+}
