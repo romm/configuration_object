a7a63d97994c4cb226bfa93ca6c2073c68a364a3
Lukas Niestroj <niestrojlukas@gmail.com>
2021-06-29 10:12:34 +0200

    [TASK] raise major version; only for TYPO3 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title'       => 'Configuration Object',
     'description' => 'Transform any configuration plain array into a dynamic and configurable object structure, and pull apart configuration handling from the main logic of your script. Use provided services to add more functionality to your objects: cache, parents, persistence and much more.',
-    'version'     => '2.0.0',
+    'version'     => '3.0.0',
     'state'       => 'stable',
     'category'    => 'backend',
 
@@ -12,12 +12,12 @@ $EM_CONF[$_EXTKEY] = [
 
     'constraints' => [
         'depends'   => [
-            'typo3' => '9.5.0-9.5.99',
-            'php'   => '7.2.0-7.99.99'
+            'typo3' => '10.4.0-10.4.99',
+            'php'   => '7.4.0-7.99.99'
         ],
         'conflicts' => [],
         'suggests'  => []
     ],
 
-    'clearCacheOnLoad' => 1
+    'clearCacheOnLoad' => true
 ];
